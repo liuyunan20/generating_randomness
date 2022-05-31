f2b34c5b89fa73ec0d97c86ebe03917cb582ab2e
liuyunan <liuyunan.20@gmail.com>
2022-05-31 01:18:34 -0700

    Read the user input the same way it did in the previous stage. Output the result in the following format: triad: counts of 0, counts of 1, for example, 000: 57,12. The result for each triad should be printed on a new line. The triads must be ordered in ascending order of their decimal representation (for example, 110 in binary = 1*4+1*2+0*1 = 6 in decimal).

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,9 +1,26 @@
+import re
+
+
 data = ''
 while len(data) < 100:
     str_in = input('Print a random string containing 0 or 1:\n')
     for x in list(str_in):
         if x in ['0', '1']:
             data += x
-    print(f'Current data length is {len(data)}, {20 - len(data)} symbols left')
-print('Final data string:')
-print(data)
+    if len(data) < 100:
+        print(f'Current data length is {len(data)}, {100 - len(data)} symbols left')
+print(f'Final data string:\n{data}\n')
+# print(data)
+triad_list = ['000', '001', '010', '011', '100', '101', '110', '111']
+triad_dict = {}
+
+for x in triad_list:
+    str_count = data
+    for _ in range(len(data)-3):
+        match = re.match(f'{x}.', str_count)
+        str_count = str_count[1:]
+        if match:
+            triad_dict.setdefault(x, [0, 0])
+            triad_dict[x][int(match.group()[-1])] += 1
+for triad in triad_list:
+    print(f'{triad}: {triad_dict[triad][0]},{triad_dict[triad][1]}')
